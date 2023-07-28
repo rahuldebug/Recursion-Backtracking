fc8c4218c7b46822f613b6d375e81fca79bdb8c4
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-07-28 17:37:51 +0530

    factorial and print

---
+++ src/main/java/recursion/Factorial.java
@@ -0,0 +1,14 @@
+package recursion;
+
+public class Factorial {
+    int factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Factorial factorial = new Factorial();
+        int result = factorial.factorial(5);
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/recursion/Print.java
@@ -0,0 +1,15 @@
+package recursion;
+
+public class Print {
+    public void printReverse(int n){
+        if(n==0) return;
+        System.out.println(n);
+        printReverse(n-1);
+        System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        Print print= new Print();
+        print.printReverse(5);
+    }
+}
