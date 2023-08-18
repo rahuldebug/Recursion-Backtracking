b7cbbb7a2382e76d06c91b92961d19ff30bfd373
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-08-19 02:40:56 +0530

    decimal to binary

---
+++ src/main/java/recursion/DecToBin.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class DecToBin {
+    static StringBuilder sb= new StringBuilder();
+    public static void main(String[] args) {
+        //System.out.println(toBinary(1048576));
+        System.out.println(toBinary(7));
+    }
+
+    public static String toBinary(int i) {
+        if(i==0) return "";
+        toBinary(i/2);
+        sb.append(i%2);
+        return sb.toString();
+    }
+}
