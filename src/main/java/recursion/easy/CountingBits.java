574d649d9fbd8baaa8b719e1fbaf82b30c22f57c
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-08-24 02:20:09 +0530

    count bits

--- src/main/java/recursion/easy/CountingBits.java
+++ src/main/java/recursion/easy/CountingBits.java
@@ -7,12 +7,15 @@ public class CountingBits {
     }
 
     private static int countBits(int n) {
+
         if (n == 0) return 0;
+        //this logic will also work but the single line logic is better
 //        if ((n & 1) == 1) {
 //            return 1 + countBits(n >> 1);
 //        } else {
 //            return 0 + countBits(n >> 1);
 //        }
+        // >> shifts right by 1 for unsigned number , where as >>> shifts right by 1 irrespective of  signed or  unsigned bit
         return (n & 1) + countBits(n >>> 1);
     }
 
